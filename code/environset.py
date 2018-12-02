3a1147d0727a4adc1388c4aa410b7e9ed8ab53f1
harry margalotti <imharrymargalotti@gmail.com>
2018-12-02 10:56:15 -0500

    stuff

---
+++ code/environset.py
@@ -0,0 +1,14 @@
+import os
+
+
+def set_pass():
+    os.environ["PASSWORD"] = 'Blazer1992'
+
+
+def set_path():
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = str('/Users/hmargalotti/Desktop/redditNLP-c250299d83e5.json')
+
+
+def set_praw():
+    os.environ['CLIENT_ID'] = "KIjD0iw7Ppx4hA"
+    os.environ['CLIENT_SECRET'] = "8CI91ITeuvjHhHH15V1thu6yDdA"
