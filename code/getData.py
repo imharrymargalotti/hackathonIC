4df0ed64d091eb74233e305ed4174c1c333831eb
tclerico <timclerico@gmail.com>
2018-12-02 01:38:41 -0500

    BIG BOI UPDATE

--- code/getData.py
+++ code/getData.py
@@ -1,8 +1,14 @@
 import praw
+import datetime
+import os
+# Imports the Google Cloud client library
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
 
-reddit = praw.Reddit(client_id='',
-                     client_secret='',
-                     user_agent='')
+reddit = praw.Reddit(client_id='KIjD0iw7Ppx4hA',
+                     client_secret='8CI91ITeuvjHhHH15V1thu6yDdA',
+                     user_agent='Get Data by /u/IthacaCompSci')
 
 print("Connection read only to reddit: " + str(reddit.read_only))
 print()
@@ -12,48 +18,78 @@ subreddit = reddit.subreddit('politics')
 
 print(subreddit.display_name)  # Output: redditdev
 
+# empty list to add all comments into
+post_comments = []
+posts = []
+
+
 # assume you have a Subreddit instance bound to variable `subreddit`
 for submission in subreddit.hot(limit=1):
-    print(submission.subreddit_id[3:])  # Output: the subreddit id
-    print(submission.id)                # Output: the post id
-    print(submission.author)            # Output: the post author
-    print(submission.created_utc)       # Output: the creation timestamp
-    print(submission.title)             # Output: the submission's title
-    print(submission.score)             # Output: the submission's score
-    print(submission.upvote_ratio)      # Output: the submission's upvote/downvote ratio
-    print(submission.url)               # Output: the URL the submission points to
-                                        # or the submission's URL if it's a self post
+    # print(submission.subreddit_id[3:])  # Output: the subreddit id
+    # print(submission.id)                # Output: the post id
+    # print(submission.author)            # Output: the post author
+    # print(submission.created_utc)       # Output: the creation timestamp
+    # print(submission.title)             # Output: the submission's title
+    # print(submission.score)             # Output: the submission's score
+    # print(submission.upvote_ratio)      # Output: the submission's upvote/downvote ratio
+    # print(submission.url)               # Output: the URL the submission points to
+    #                                     # or the submission's URL if it's a self post
+    post_info = {
+        "subreddit_id" : submission.subreddit_id[3:],
+        "subreddit": subreddit.display_name,
+        "post_id" : submission.id,
+        "author": submission.author,
+        "author_id": reddit.redditor(str(submission.author)).id,
+        "date": datetime.datetime.fromtimestamp(submission.created_utc).strftime('%Y-%m-%d %H:%M:%S'),
+        "title": submission.title,
+        "karma": submission.score,
+        "vote_ratio": submission.upvote_ratio,
+        "link": submission.url
+    }
+    print(post_info)
+    posts.append(post_info)
 
-    print()
 
     submission.comments.replace_more(limit=None)
     for comment in submission.comments.list():
         if comment.body != "[removed]":
-            print("post id: " + str(comment.submission.id))
-            print("comment id: " + str(comment.id))
-            print("permalink: " + str(comment.permalink))
-            print("author: " + str(comment.author))
-            print("author id: " + str(reddit.redditor(str(comment.author)).id))
-            print("created time: " + str(comment.created_utc))
-            print("body: " + str(comment.body))
-            print("score: " + str(comment.score))
-            if str(comment.parent_id)[:2] != "t3":
-                print("parent id: " + str(comment.parent_id)[3:])
-            else:
-                print("parent id: null")
-            print()
+            # pid = print("post id: " + str(comment.submission.id))
+            # print("comment id: " + str(comment.id))
+            # print("permalink: " + str(comment.permalink))
+            # print("author: " + str(comment.author))
+            # print("author id: " + str(reddit.redditor(str(comment.author)).id))
+            # print("created time: " + str(comment.created_utc))
+            # print("body: " + str(comment.body))
+            # print("score: " + str(comment.score))
+            # if str(comment.parent_id)[:2] != "t3":
+            #     print("parent id: " + str(comment.parent_id)[3:])
+            # else:
+            #     print("parent id: null")
+            # print()
+
+            # get all info from comments and add to list
+            info = {
+                'post_id': comment.submission.id,
+                'comment_id': comment.id,
+                'permalink': comment.permalink,
+                'author': comment.author,
+                'author_id': reddit.redditor(str(comment.author)).id,
+                'date': datetime.datetime.fromtimestamp(comment.created_utc).strftime('%Y-%m-%d %H:%M:%S'),
+                'body': comment.body,
+                'score': comment.score,
+                'parent_id': (comment.parent_id if str(comment.parent_id)[:2] != "t3" else 'NULL')
+            }
+            post_comments.append(info)
+
+    print(post_comments)
 
     print()
 
 #this is harrys section do not write below this only above
 
-import os
-# Imports the Google Cloud client library
-from google.cloud import language
-from google.cloud.language import enums
-from google.cloud.language import types
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = str('/Users/harrymargalotti/Desktop/redditNLP-c250299d83e5.json')
+
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = str('/Users/timc/Desktop/redditNLP-c250299d83e5.json')
 # Instantiates a client
 client = language.LanguageServiceClient()
 
@@ -68,3 +104,23 @@ sentiment = client.analyze_sentiment(document=document).document_sentiment
 
 print('Text: {}'.format(text))
 print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
+
+for p in posts:
+    text = p.get("title")
+    document = types.Document(
+        content = text,
+        type = enums.Document.Type.PLAIN_TEXT
+    )
+    sentiment = client.analyze_sentiment(document=document).document_sentiment
+    p["sentiment"] = sentiment
+
+for c in post_comments:
+    text = c.get("body")
+    document = types.Document(
+        content=text,
+        type=enums.Document.Type.PLAIN_TEXT
+    )
+    sentiment = client.analyze_sentiment(document=document).document_sentiment
+    c["sentiment"] = sentiment
+
+
--- code/mysqlstuff.py
+++ code/mysqlstuff.py
@@ -11,3 +11,55 @@ except mysql.connector.Error as err:
         print("database doesn't exist")
     else:
         print(err)
+
+
+def insert_post(info):
+    # create sql query
+    cursor = cnx.cursor()
+
+    # create user if user id doesn't exist
+    try:
+        sql = "INSERT INTO Users (id, name) VALUES (%s, %s)"
+        val = (info.get('author_id'), info.get('author'))
+        cursor.execute(sql, val)
+    except:
+        print("User exists")
+
+    # create subreddit if it doesn't already exist
+    try:
+        sql = "INSERT INTO SubReddits (id, name) VALUES (%s, %s)"
+        val = (info.get('subreddit_id'), info.get('subreddit'))
+        cursor.execute(sql, val)
+    except:
+        print("Subreddit exists")
+
+    # insert post
+    sql = "INSERT INTO Posts (id, title, date, link, sentiment, karma, user_id, subreddit_id, subject_id ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    val = (info.get('post_id'), info.get('title'), info.get('date'), info.get('link'), info.get('sentiment'),
+           info.get('karma'), info.get('author_id'), info.get('subreddit_id'), info.get('subject_id'))
+
+    cursor.execute(sql, val)
+
+    cnx.commit()
+
+def insert_comment(info):
+    # create sql query
+    cursor = cnx.cursor()
+
+    # create user if user id doesn't exist
+    try:
+        sql = "INSERT INTO Users (id, name) VALUES (%s, %s)"
+        val = (info.get('author_id'), info.get('author'))
+        cursor.execute(sql, val)
+    except:
+        print("User exists")
+
+    # insert comment
+    sql = "INSERT INTO Comments (id, body, date, link, karma, sentiment, user_id, post_id, subject_id, parent_id ) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    val = (info.get('comment_id'), info.get('bosy'), info.get('date'), info.get('permalink'), info.get('score'),
+           info.get('sentiment'), info.get('author_id'), info.get('post_id'), info.get('subject_id'), info.get('parent_id'))
+
+    cursor.execute(sql, val)
+
+    cnx.commit()
+
