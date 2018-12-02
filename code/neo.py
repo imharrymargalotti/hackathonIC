a89bdc1c1c71a2f01e675efdb927d6c6ae122853
tclerico <timclerico@gmail.com>
2018-12-01 21:38:45 -0500

    neo stuff

---
+++ code/neo.py
@@ -0,0 +1,28 @@
+from neo4j.v1 import GraphDatabase
+
+uri = "bolt://localhost:7687"
+driver = GraphDatabase.driver(uri, auth=("neo4j", "Blazer1992"))
+
+def add_friend(tx, name, friend_name):
+    tx.run("MERGE (a:Person {name: $name}) "
+           "MERGE (a)-[:KNOWS]->(friend:Person {name: $friend_name})",
+           name=name, friend_name=friend_name)
+
+
+def print_friends(tx, name):
+    for record in tx.run("MATCH (a:Person)-[:KNOWS]->(friend) WHERE a.name = $name "
+                         "RETURN friend.name ORDER BY friend.name", name=name):
+        print(record["friend.name"])
+
+
+def create_test(tx, name):
+    tx.run("MERGE (a:Test {name: $name})", name=name)
+
+
+with driver.session() as session:
+    session.write_transaction(create_test, "Tim")
+    # session.write_transaction(add_friend, "Arthur", "Guinevere")
+    # session.write_transaction(add_friend, "Arthur", "Lancelot")
+    # session.write_transaction(add_friend, "Arthur", "Merlin")
+    # session.read_transaction(print_friends, "Arthur")
+    # session.write_transaction(create_test, "Tim")
